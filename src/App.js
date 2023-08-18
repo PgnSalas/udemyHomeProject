a2c34d25a038236bfe5812dd51540bdbe24db80b
Gleb <gjivora@gmail.com>
2023-08-18 21:11:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h4>Gleb is World</h4>
         <a
           className="App-link"
           href="https://reactjs.org"
